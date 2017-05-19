b3253ea5d54ac1d3eaa37046eec98ec93141b5e2
Marvin <marvin.reese@web.de>
2017-05-19 14:51:44 +0200

    1 - loginActivity: set focus to first textEdit 2 - created resources files colors.xml, strings.xml, styles.xml in package values !strings should not be hardcoded to layout files, but referenced as static variables in this package! package just created, going to implement the static references soon

--- app/src/main/java/enterprise/lgm/jabber/LoginActivity.java
+++ app/src/main/java/enterprise/lgm/jabber/LoginActivity.java
@@ -11,6 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.editText4).requestFocus();
     }
     public void gotoLogin(View v)
     {
