6d556dda755ded2a7e4e2aac60b3ff466d9b5ed9
Marvin <marvin.reese@web.de>
2017-05-26 16:10:56 +0200

    a bit of refactoring and adding functionality to EditTexts in RegisterActivity  -> you can now type your desired nickname and pw to register (works without response on UI)

--- app/src/main/java/enterprise/lgm/jabber/RegisterActivity.java
+++ app/src/main/java/enterprise/lgm/jabber/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.IOException;
 
@@ -15,26 +16,34 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
         Button btnRegister = (Button) findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                register("tf", "fsfscstest");
+
+                EditText nickname = (EditText) findViewById(R.id.registerNickname);
+                String user = nickname.getText().toString();
+
+                EditText password1 = (EditText) findViewById(R.id.registerPassword);
+                String pw1 = password1.getText().toString();
+
+                EditText password2 = (EditText) findViewById(R.id.registerPasswordRepeat);
+                String pw2 = password2.getText().toString();
+
+                if (pw1.equals(pw2)) {
+                    try {
+                        Server.getServer().register(user, pw1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }
         });
     }
 
-    public void register(String user, String pw){
+    protected void method() {
 
-        Server s = new Server();
-        try {
-            if(s.register(user, pw)==200){
-                System.out.println("erfolgreich registriert");
-            }else{
-                System.out.println("registrierung nicht erfolgreich");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- app/src/main/java/enterprise/lgm/jabber/Server.java
+++ app/src/main/java/enterprise/lgm/jabber/Server.java
@@ -12,6 +12,24 @@ import java.util.*;
 
 public class Server {
 
+    //making the server a singleton (allows only one instance of the class Server)
+    //Server instance can be accessed from everywhere with Server.getServer()
+    private static Server server;
+
+    private Server() {
+
+    }
+
+    public static Server getServer() {
+        if (Server.server == null) {
+            Server.server = new Server();
+        }
+        return Server.server;
+    }
+
+
+    //methods for server related functions
+
     public int register(final String user, final String pw) throws IOException {
         final int[] answer = new int[1];
         Thread t = new Thread(new Runnable() {
@@ -21,6 +39,53 @@ public class Server {
                 try {
                     url = new URL("http://palaver.se.paluno.uni-due.de/api/user/register");
 
+                    String par = "{\"Username\":\"" + user + "\",\"Password\":\"" + pw + "\"}";
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    conn.setDoOutput(true);
+
+                    OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+                    writer.write(par);
+                    writer.flush();
+                    //  Log.v("", "" + conn.getResponseCode());
+
+                    Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                    String ausgabe = "";
+                    for (int c; (c = in.read()) >= 0; )
+                        ausgabe += (char) c;
+                    System.out.println(ausgabe);
+
+
+                    answer[0] = in.read();
+                    System.out.println("in read ist: "+answer[0]);
+
+                } catch (UnsupportedEncodingException e1) {
+                    e1.printStackTrace();
+                } catch (ProtocolException e1) {
+                    e1.printStackTrace();
+                } catch (MalformedURLException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+        });
+        t.start();
+
+        return answer[0];
+    }
+
+
+    public int login(final String user, final String pw) throws IOException {
+        final int[] answer = new int[1];
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                URL url = null;
+                try {
+                    url = new URL("http://palaver.se.paluno.uni-due.de/api/user/validate");
 
                     String par = "{\"Username\":\"" + user + "\",\"Password\":\"" + pw + "\"}";
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -28,8 +93,7 @@ public class Server {
                     conn.setRequestProperty("Content-Type", "application/json");
                     conn.setDoOutput(true);
 
-                    OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(
-                    ));
+                    OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
                     writer.write(par);
                     writer.flush();
                     //  Log.v("", "" + conn.getResponseCode());
